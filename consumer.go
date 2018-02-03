eb1df7fb4f4187781dfc9a80b868a7ce3d217a67
Nadir Saleh <Nadir@Nadirs-Mac.local>
2018-02-03 18:41:19 -0500

    just some ideas

--- consumer.go
+++ consumer.go
@@ -9,15 +9,61 @@ import (
 )
 
 func ConsumeMatchData(body []byte) error {
-	var match obj.MatchDto
-	err := json.Unmarshal(body, &match)
+	var matchData obj.MatchData
+	err := json.Unmarshal(body, &matchData)
 	if err != nil {
 		return errors.Wrap(err, "Error unmarshalling to type MatchDto in ConsumeMatchData")
 	}
 
-	log := fmt.Sprintf("MatchData: %v", match)
+	summonerIdentity, err := FindSummonerIdentity(matchData)
+	if err != nil {
+		return errors.Wrap(err, "Error in ConsumeMatchData")
+	}
+
+	summonerStats, err := FindSummonerStats(matchData, summonerIdentity)
+	if err != nil {
+		return errors.Wrap(err, "Error in ConsumeMatchData")
+	}
+
+	log := fmt.Sprintf("MatchData: %v", summonerStats)
 
 	fmt.Println(log)
 
 	return nil
+}
+
+func FindSummonerStats(data obj.MatchData, summonerIdentity *obj.ParticipantIdentityDto) (*obj.ParticipantStatsDto, error) {
+	match := data.Match
+
+	stats := new(obj.ParticipantStatsDto)
+
+	for _, participant := range match.Participants {
+		if participant.ParticipantId == summonerIdentity.ParticipantId {
+			stats = &participant.Stats
+		}
+	}
+
+	if stats == nil {
+		return nil, errors.New("Could not find summonerStats in FindSummonerStats")
+	}
+
+	return stats, nil
+}
+
+func FindSummonerIdentity(data obj.MatchData) (*obj.ParticipantIdentityDto, error) {
+	participants := data.Match.ParticipantIdentities
+	identity := new(obj.ParticipantIdentityDto)
+
+	for _, participant := range participants {
+		if participant.Player.SummonerName == data.SummonerName {
+			identity.Player = participant.Player
+			identity.ParticipantId = participant.ParticipantId
+		}
+	}
+
+	if identity == nil {
+		return nil, errors.New("Could not find summoner in participant identities")
+	}
+
+	return identity, nil
 }
---
+++ objects/data.go
@@ -0,0 +1,6 @@
+package objects
+
+type MatchData struct {
+	Match        MatchDto `json:"match"`
+	SummonerName string   `json:"summonerName"`
+}
